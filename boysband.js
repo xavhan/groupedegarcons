5a87d439ab69a0546ee63a7d6c0093820f7aa087
Xavier Haniquaut <xavier.haniquaut@gmail.com>
2025-07-31 08:02:49 +0200

    Start

---
+++ boysband.js
@@ -0,0 +1,56 @@
+export const DATABASE = [
+  {
+    boysband: "Allan Théo",
+    track: "Emmène moi",
+    yt: "aOTcm7_mLIE",
+    year: 1997,
+  },
+  // {
+  //   boysband: "Benoit",
+  //   track: "Tourne-toi Benoit",
+  //   yt: "GLi-UF9U-RI",
+  //   year: 2002,
+  // },
+  {
+    boysband: "G-Squad",
+    track: "Raide dingue de toi",
+    yt: "wtwublQJceQ",
+    year: 1996,
+  },
+  {
+    boysband: "Alliage",
+    track: "Le temps qui court",
+    yt: "fWV3d3yVZ8E",
+    year: 1997,
+  },
+  {
+    boysband: "2 Be 3",
+    track: "2 Be 3",
+    yt: "CypDICCglYk",
+    year: 1997,
+  },
+  {
+    boysband: "2 Be 3",
+    track: "Toujours là pour toi",
+    yt: "vzKyGv_Pv4s",
+    year: 1997,
+  },
+  {
+    boysband: "Poetic Lover",
+    track: "Prenons notre temps",
+    yt: "kOW4KhdwlrU",
+    year: 1997,
+  },
+  {
+    boysband: "Poetic Lover",
+    track: "Qu'il en soit ainsi",
+    yt: "SYCTOT9PftM",
+    year: 1997,
+  },
+  {
+    boysband: "Worlds Apart",
+    track: "Je te donne",
+    yt: "kxsFwoh3cik",
+    year: 1996,
+  },
+];
